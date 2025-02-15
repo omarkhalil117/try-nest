157d53df35663a65162542080bd299255b32c323
Omar Sayed <omarkhalil117@gmail.com>
2025-02-15 20:39:37 +0200

    feat: return jwt token on login

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { TasksModule } from './tasks/tasks.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { AuthModule } from './auth/auth.module';
     UsersModule,
     MongooseModule.forRoot('mongodb://localhost/nestdb'),
     AuthModule,
+    ConfigModule.forRoot(),
   ],
 })
 export class AppModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,10 +7,14 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/users/user.schema';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>,
+    private jwtService: JwtService,
+  ) {}
 
   async signIn(username: string, password: string): Promise<any> {
     const user = await this.userModel.findOne({ name: username });
@@ -25,6 +29,8 @@ export class AuthService {
       throw new UnauthorizedException('Invalid credentials.');
     }
 
-    return 'logged in !';
+    const payload = { id: user._id, name: username };
+
+    return { access_token: await this.jwtService.signAsync(payload) };
   }
 }
